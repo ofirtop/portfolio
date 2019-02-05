c6c2162cc0068074da20ef27fe1a69bb6da6f7e8
ofirtop <ofirtopaz@gmail.com>
2019-02-05 08:49:15 +0200

    offCanvas email functionality

--- js/offCanvas.js
+++ js/offCanvas.js
@@ -2,3 +2,11 @@ function openCanvas(){
     document.querySelector('.offcanvas-btn').classList.toggle('offcanvas-btn-open');
     document.querySelector('.offcanvas-aside').classList.toggle('offcanvas-aside-open');    
 }
+
+function onSubmit(){
+    var subject = $('.subject-section-input').val();
+    var email = $('.email-section-input').val();
+    var msg = $('.textarea-section-input').val();
+    window.location = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${msg}`;
+    
+}
